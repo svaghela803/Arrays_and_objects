9a8ed4170ab3f14365bb0dc29ea42c63bd06ee10
Shivangi <shivangi_vaghela@hotmail.co.uk>
2022-09-20 15:23:14 +0100

    q2 objects

--- objects.js
+++ objects.js
@@ -37,9 +37,9 @@ console.log("The dog's name is " + dogName);
  * Q2. What breed is the cat?
  */
 
-catBreed = null;
+catBreed = cat.breed;
 
-// console.log("The cat is a " + catBreed);
+console.log("The cat is a " + catBreed);
 
 
 
